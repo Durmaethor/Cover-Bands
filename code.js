ff6e0c353786643fda3686e3201df629b0a5aa29
Ryan Kramer <rkramer613@gmail.com>
2017-01-17 18:28:22 -0700

    Added cover bands

---
+++ code.js
@@ -0,0 +1,41 @@
+var coverBands = [
+    {
+        bandName: 'Living Putsteaks',
+        contact: 'dave@livingputsteaks.com',
+        covering: 'Yard Birds'
+    },
+    {
+        bandName: 'Petty Theft',
+        contact: 'steve@pettytheft.com ',
+        covering: 'Tom Petty' 
+    },
+    {
+        bandName: 'Metalachi',
+        contact: 'arriche@metalachi.com',
+        covering: 'Metalica'
+    },
+    {
+        bandName: 'Led Zeppagain',
+        contact: 'Mike@zeppagain.com',
+        covering: 'Journey'
+    },
+    {
+        bandName: 'Heartbreakers',
+        contact: 'breakers@breakers.com',
+        covering: 'Tom Petty'
+    },
+    {
+        bandName: 'Arnel',
+        contact: 'Arnel@littlepinodude.com',
+        covering: 'Journey'
+    },
+    {
+        bandName: 'Distance',
+        contact: 'gothedistance@distance.com',
+        covering: 'Journey'
+    }
+]
+
+var expectedResult = {
+    
+}
